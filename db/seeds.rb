bcc93c8d17b4f119118321911dde2e084de4a8aa
Eric Chiu <c3366301@gmail.com>
2018-07-15 10:10:55 +0800

    add seed: create_League_default_data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Starting add db seeds...'
+puts "這個種子檔會自動創建 league definitions"
+
+League.find_or_create_by!({name: "NBA", official_site: "https://data.nba.net/prod/v1/2018/teams.json"})
+League.find_or_create_by!({name: "MLB", official_site: "https://www.si.com/mlb/"})
